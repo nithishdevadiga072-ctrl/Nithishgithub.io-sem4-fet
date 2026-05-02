a3a80bddfec046d494f6119623964f83c479d0d7
nithishdevadiga072-ctrl <nithishdevadiga072@gmail.com>
2026-05-02 10:26:22 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -1,14 +1,4 @@
-<div class="container">{
-        <img src="https://via.placeholder.com/150" alt="Profile Picture" class="profile-img">
-        
-        <h1 id="name">Hello, I'm Nithish Devadiga</h1>
-        <p class="bio">
-            I am a web developer passionate about creating simple and beautiful websites.
-        </p>
 
-        <button onclick="showMessage()">Click to Know More</button>
-
-        <p id="extra-info"></p> }
     function showMessage() {
     // Ask user name
     let name = prompt("What is your name?");
