6b9500ab859bac60c143111718a5a81f1abda563
nithishdevadiga072-ctrl <nithishdevadiga072@gmail.com>
2026-05-02 09:43:06 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -1,1 +1,17 @@
+function showMessage() {
+    // Ask user name
+    let name = prompt("What is your name?");
 
+    // Ask user interest
+    let interest = prompt("What do you like to do?");
+
+    // Show greeting message
+    alert("Hello " + name + "! 😊");
+
+    // Show interest message
+    alert("That's awesome! I also like " + interest + "!");
+
+    // Display on page
+    document.getElementById("extra-info").innerText =
+        "Nice to meet you, " + name + "! You enjoy " + interest + ".";
+}
